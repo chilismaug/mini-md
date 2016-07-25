befc60398d9eef4746e50e1dcf11bb58ab05748e
charles ross <charles.ross.oh@gmail.com>
2016-07-24 21:19:37 -0400

    reworked menu to move files and view to each own menu column

--- kyles-custom-menu.js
+++ kyles-custom-menu.js
@@ -4,31 +4,61 @@ const Menu = remote.require('menu')
 const win = remote.win
 
 const menu = Menu.buildFromTemplate([
-  {
-    label: 'Electron',
-    submenu: [
     {
-      label: 'Prefs',
-      click: function () {
-        ipc.send('toggle-prefs')
-      }
-      }, {
-        label: 'Open File',
-        accelerator: "Command+O",
-        click:   function () {
-          ipc.send('open-file-dialog')
+      label: 'Electron',
+      submenu: [    
+        {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click: function () {
+            remote.require('app').quit()
+            }
+         }]
+    },{
+      label: 'File',
+      submenu: [ 
+        {  
+            label: 'Open File',
+            accelerator: "Command+O",
+            click:   function () {
+              ipc.send('open-file-dialog')
+            } 
+         }]
+        }, { 
+            type: 'separator'
+        }, {
+            label: 'View',
+            submenu: [{
+              label: 'Toggle Full Screen',
+              accelerator: (function () {
+                if (process.platform === 'darwin') {
+                  return 'Ctrl+Command+F'
+                } else {
+                  return 'F11'
+                }
+              })(),
+              click: function (item, focusedWindow) {
+                if (focusedWindow) {
+                  focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+                }
+              }
+            }, {
+              label: 'Toggle Developer Tools',
+              accelerator: (function () {
+                if (process.platform === 'darwin') {
+                  return 'Alt+Command+I'
+                } else {
+                  return 'Ctrl+Shift+I'
+                }
+              })(),
+              click: function (item, focusedWindow) {
+                if (focusedWindow) {
+                  focusedWindow.toggleDevTools()
+                }
+              }
+            }]
         }
-      }, { 
-        type: 'separator'
-      }, {
-        label: 'Quit',
-        accelerator: 'Command+Q',
-        click: function () {
-          remote.require('app').quit()
-        }
-      }
-    ]
-  }
+    
 ])
 Menu.setApplicationMenu(menu)
  
